9f242718652cf75b4ac701ee9a81eefd5aee973d
devesh-d16 <dewangandevesh1602@gmail.com>
2024-05-21 15:25:38 +0530

    initial commit

---
+++ Mosaik Backend/src/main/java/tes/mosaik/MosaikApplication.java
@@ -0,0 +1,13 @@
+package tes.mosaik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosaikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosaikApplication.class, args);
+	}
+
+}
---
+++ Mosaik Backend/src/test/java/tes/mosaik/MosaikApplicationTests.java
@@ -0,0 +1,13 @@
+package tes.mosaik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosaikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
