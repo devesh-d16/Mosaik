d77d72f9097175e3cf7de31b84f66540f77884d9
devesh-d16 <dewangandevesh1602@gmail.com>
2024-05-29 03:30:36 +0530

    login/signup page implemented

--- Mozaik Frontend/src/App.jsx
+++ Mozaik Frontend/src/App.jsx
@@ -5,20 +5,27 @@ import Navbar from "./pages/Navbar/Navbar";
 import ProjectDetails from "./pages/ProjectDetails/ProjectDetails";
 import IssueDetails from "./pages/IssueDetails/IssueDetails";
 import Subscription from "./pages/Subscription/Subscription";
+import Auth from "./pages/Project/Auth/Auth";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/project/:id" element={<ProjectDetails />} />
-        <Route
-          path="/project/:projectID/issue/:issueID"
-          element={<IssueDetails />}
-        />
-        <Route path="/upgrade_plan" element={<Subscription />} />
-      </Routes>
+      {false ? (
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/project/:id" element={<ProjectDetails />} />
+            <Route
+              path="/project/:projectID/issue/:issueID"
+              element={<IssueDetails />}
+            />
+            <Route path="/upgrade_plan" element={<Subscription />} />
+          </Routes>
+        </div>
+      ) : (
+        <Auth />
+      )}
     </>
   );
 }
